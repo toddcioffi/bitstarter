2fe7b95d68a281b105773c93eeff590d631a05c9
Todd Cioffi <todd.cioffi@gmail.com>
2013-07-25 14:39:31 -0400

    Re-updated web.js to reflect new 'express' format

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-#var app = express.createServer(express.logger());
+/*var app = express.createServer(express.logger());*/
 var app = express();
 
 app.get('/', function(request, response) {
